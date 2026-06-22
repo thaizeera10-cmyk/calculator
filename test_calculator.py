6a873938e98df815824479d541322a1246acd6e9
Thaizeera <thaizeera10@gmail.com>
2026-06-22 11:21:06 +0530

    update tests and workflow

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import add
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, 2), 1)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
 
